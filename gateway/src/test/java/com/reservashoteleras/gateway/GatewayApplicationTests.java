30f0805d3165cac084eea454d3e470a8191cef65
Angel Apolinar <angelapolinaroregon.2002@gmail.com>
2026-07-24 18:48:56 -0600

    Angel Eureka y Gateway

--- gateway/src/test/java/com/reservashoteleras/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/com/reservashoteleras/gateway/GatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package com.reservashoteleras.Eureka;
+package com.reservashoteleras.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaApplicationTests {
+class GatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
